216ceb0706e2f35bafb4db815fea7f4662cb03de
Golden-Lizard <sandro_void_necro@yahoo.com>
2016-08-07 13:58:47 +0300

    Update BP_functor.cpp

--- AtT_installer/public/BP_functor.cpp
+++ AtT_installer/public/BP_functor.cpp
@@ -8,5 +8,10 @@ _back_up<size_t size>::_back_up()
 template<size_t size>
 _back_up<size_t size> const & _back_up<size_t size>::operator()(const _file_ & _file_to_bck_up, wchar_t(&_backup_path)[size])
 {
-	return 0;
+	/*TO DO
+	*copy file to backup folder
+	*rename extension to .bak
+	*remove original file
+	*/
+    return 0;
 };
